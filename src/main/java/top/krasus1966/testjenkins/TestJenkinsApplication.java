53d6ef019f712688358f4c3816d5f92477496a21
chenxu <2134123985@qq.com>
2023-09-17 18:35:31 +0800

    测试提交

--- src/main/java/top/krasus1966/testjenkins/TestJenkinsApplication.java
+++ src/main/java/top/krasus1966/testjenkins/TestJenkinsApplication.java
@@ -2,12 +2,19 @@ package top.krasus1966.testjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestJenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestJenkinsApplication.class, args);
     }
 
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
 }
